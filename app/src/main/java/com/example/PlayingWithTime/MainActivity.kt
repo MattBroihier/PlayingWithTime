4a0153a9434ece37f916bc1f734146af4acf78f4
MattBroihier <mattbroihier@gmail.com>
2021-01-24 10:25:41 -0600

    Trying to learn Kotlin from the ground up, doing a few simple things, and continuing to push further

---
+++ app/src/main/java/com/example/PlayingWithTime/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.PlayingWithTime
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+//        findViewById<Button>(R.id.TheButtonThatDoesSomething).setOnClickListener{
+//            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+//        }
+//        val button = findViewById(R.id.TheButtonThatDoesSomething) as Button
+//        // get reference to button
+//        button.setOnClickListener {
+//            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+//        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun ButtonActionThing(view: View) {
+        view.setOnClickListener(
+            Toast.makeText(this@MainActivity, "You clicked me again.", Toast.LENGTH_SHORT).show()
+        )
+    }
+}
+
+private fun View.setOnClickListener(show: Unit) {
+
+}
